6326199eec2e136b0362849db7c4531705901a59
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-23 22:48:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,18 @@ class Person
     def initialize(attributes)
       attributes.each {|key, value| self.send(("#{key}="), value)}
     end
+    
+     def add_attributes(attributes)
+    @collar = collar
+    @toys = toys
+  end
+  
 end
+
+new_dog = Dog.new()
+new_dog.add_attributes(collar:"red",toys:"ball")
+new_dog.collar
+e
 
 spencer = User.new
 sophie.send("#{method_name}=", value)
